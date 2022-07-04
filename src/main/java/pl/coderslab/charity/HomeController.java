72ee6b9fd4e1076b0989c36199fffb4ed455c3a1
Piotr Pucołowski <p.pucolowski@gmail.com>
2022-07-04 17:41:21 +0200

    HomeController

--- src/main/java/pl/coderslab/charity/HomeController.java
+++ src/main/java/pl/coderslab/charity/HomeController.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.charity.model.Donation;
+import pl.coderslab.charity.repository.CategoryRepository;
 import pl.coderslab.charity.repository.DonationRepository;
 import pl.coderslab.charity.repository.InstitutionRepository;
 
@@ -15,11 +17,13 @@ public class HomeController {
 
     private final DonationRepository donationRepository;
     private final InstitutionRepository institutionRepository;
+    private final CategoryRepository categoryRepository;
 
     @RequestMapping("/")
     public String homeAction(Model model) {
         model.addAttribute("bags", donationRepository.sumAllByQuantity());
-        model.addAttribute("donations", donationRepository.countAllDonation());
+        model.addAttribute("donations", donationRepository.count());
+        model.addAttribute("categories", categoryRepository.findAll());
         model.addAttribute("institutions", institutionRepository.findAll());
         return "index";
 
@@ -54,15 +58,15 @@ public class HomeController {
     public  String register(){
         return "register";
     }
-    @GetMapping("/form")
-    public String getDonation(){
-        return "form";
-    }
-    @GetMapping("/form-confirmation")
+    @GetMapping("/formConfirmation")
         public String formConfirmation(){
         return "formConfirmation";
     }
 
+    @GetMapping("/stats")
+    public String stats(){
+        return "stats";
+    }
 
 
 }
